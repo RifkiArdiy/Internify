2c962d2f6fc05365013824e8c8f501b01b24f571
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-05-05 21:04:38 +0700

    edit company pages

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,43 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>Daftar Perusahaan Mitra</h2>
-    <a href="{{ route('companies.create') }}" class="btn btn-primary mb-3" style="float:right;">Tambah Perusahaan</a>
-
     @if (session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
-
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th style="background: rgb(76, 76, 210); color:white">Nama Perusahaan</th>
-                <th>Bidang Industri</th>
-                <th>Alamat</th>
-                <th>Kontak</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody >
-            @foreach ($companies as $company)
-                <tr>
-                    <td>{{ $company->name }}</td>
-                    <td>{{ $company->industry }}</td>
-                    <td>{{ $company->address }}</td>
-                    <td>{{ $company->contact }}</td>
-                    <td>
-                        <a href="{{ route('companies.show', $company->company_id) }}" class="btn btn-sm btn-info">Detail</a>
-                        <a href="{{ route('companies.edit', $company->company_id) }}" class="btn btn-sm btn-warning">Edit</a>
-                        <form action="{{ route('companies.destroy', $company->company_id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus perusahaan ini?')">Hapus</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <div class="card card-bordered card-preview">
+        <div class="card-inner">
+            <table class="datatable-init-export nowrap nk-tb-list nk-tb-ulist" data-auto-responsive="false">
+                <thead>
+                    <tr class="nk-tb-item nk-tb-head">
+                        <th class="nk-tb-col nk-tb-col-check">
+                            <div class="custom-control custom-control-sm custom-checkbox notext">
+                                <input type="checkbox" class="custom-control-input" id="uid">
+                                <label class="custom-control-label" for="uid"></label>
+                            </div>
+                        </th>
+                        <th class="nk-tb-col"><span class="sub-text">Perusahaan</span></th>
+                        <th class="nk-tb-col"><span class="sub-text">Bidang Industri</span></th>
+                        <th class="nk-tb-col"><span class="sub-text">Alamat</span></th>
+                        <th class="nk-tb-col"><span class="sub-text">Kontak</span></th>
+                        <th class="nk-tb-col nk-tb-col-tools text-end"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($companies as $company)
+                        <tr class="nk-tb-item">
+                            <td class="nk-tb-col nk-tb-col-check">
+                                <div class="custom-control custom-control-sm custom-checkbox notext">
+                                    <input type="checkbox" class="custom-control-input" id="uid{{ $company->company_id }}">
+                                    <label class="custom-control-label" for="uid{{ $company->company_id }}"></label>
+                                </div>
+                            </td>
+                            <td class="nk-tb-col">
+                                <div class="user-card">
+                                    <div class="user-avatar bg-dark d-none d-sm-flex">
+                                        <img src="{{ asset('assets/home/images/team/a.jpg') }}" alt="company-logo">
+                                    </div>
+                                    <div class="user-info">
+                                        <span class="tb-lead">{{ $company->name }}<span class="dot dot-success d-md-none ms-1"></span></span>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="nk-tb-col">
+                                <span>{{ $company->industry }}</span>
+                            </td>
+                            <td class="nk-tb-col">
+                                <span>{{ $company->address }}</span>
+                            </td>
+                            <td class="nk-tb-col">
+                                <span>{{ $company->contact }}</span>
+                            </td>
+                            <td class="nk-tb-col nk-tb-col-tools">
+                                <ul class="nk-tb-actions gx-1">
+                                    <li>
+                                        <a href="{{ route('companies.show', $company->company_id) }}" class="btn btn-trigger btn-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Detail">
+                                            <em class="icon ni ni-eye"></em>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('companies.edit', $company->company_id) }}" class="btn btn-trigger btn-icon" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
+                                            <em class="icon ni ni-repeat"></em>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <form action="{{ route('companies.destroy', $company->company_id) }}" method="POST" class="d-inline" onsubmit="return confirm('Yakin ingin menghapus perusahaan ini?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-trigger btn-icon btn-danger" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus">
+                                                <em class="icon ni ni-trash"></em>
+                                            </button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
