ee7d8e988367a4ec0fd9b7ca2bdd2b9b29c54537
RifkiArdiy <ardiyrifki@gmail.com>
2025-05-05 20:44:58 +0700

    update(view) mengubah tampilan template sementara

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,67 @@
+@if (Auth::user()->level->level_nama == 'admin')
+    <li class="nk-menu-heading">
+        <h6 class="overline-title text-primary-alt">Dashboards</h6>
+    </li>
+    <!-- .nk-menu-heading -->
+    <li class="nk-menu-item">
+        <a href="{{ route('admin.dashboard') }}" class="nk-menu-link">
+            <span class="nk-menu-icon">
+                <em class="icon ni ni-dashlite"></em>
+            </span>
+            <span class="nk-menu-text">Dashboard</span>
+        </a>
+    </li>
+    <!-- .nk-menu-item -->
+    <li class="nk-menu-heading">
+        <h6 class="overline-title text-primary-alt">Pages</h6>
+    </li>
+    <!-- .nk-menu-heading -->
+    <li class="nk-menu-item has-sub">
+        <a href="{{ route('mahasiswa.index') }}" class="nk-menu-link">
+            <span class="nk-menu-icon">
+                <em class="icon ni ni-users"></em>
+            </span>
+            <span class="nk-menu-text">Mahasiswa</span>
+        </a>
+        <!-- .nk-menu-sub -->
+    </li>
+    <li class="nk-menu-item has-sub">
+        <a href="{{ route('dosen.index') }}" class="nk-menu-link">
+            <span class="nk-menu-icon">
+                <em class="icon ni ni-users"></em>
+            </span>
+            <span class="nk-menu-text">Dosen</span>
+        </a>
+        <!-- .nk-menu-sub -->
+    </li>
+@endif
+@if (Auth::user()->level->level_nama == 'dosen')
+    <li class="nk-menu-heading">
+        <h6 class="overline-title text-primary-alt">Dashboards</h6>
+    </li>
+    <!-- .nk-menu-heading -->
+    <li class="nk-menu-item">
+        <a href="{{ route('dosen.dashboard') }}" class="nk-menu-link">
+            <span class="nk-menu-icon">
+                <em class="icon ni ni-dashlite"></em>
+            </span>
+            <span class="nk-menu-text">Dashboard</span>
+        </a>
+    </li>
+    <!-- .nk-menu-item -->
+@endif
+@if (Auth::user()->level->level_nama == 'mahasiswa')
+    <li class="nk-menu-heading">
+        <h6 class="overline-title text-primary-alt">Dashboards</h6>
+    </li>
+    <!-- .nk-menu-heading -->
+    <li class="nk-menu-item">
+        <a href="{{ route('mahasiswa.dashboard') }}" class="nk-menu-link">
+            <span class="nk-menu-icon">
+                <em class="icon ni ni-dashlite"></em>
+            </span>
+            <span class="nk-menu-text">Dashboard</span>
+        </a>
+    </li>
+    <!-- .nk-menu-item -->
+@endif
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -9,7 +9,7 @@
             </a>
         </div>
         <div class="nk-sidebar-brand">
-            <a href="html/index.html" class="logo-link nk-sidebar-logo">
+            <a href="{{ route('welcome.index') }}" class="logo-link nk-sidebar-logo">
                 <img class="logo-light logo-img" src="{{ asset('assets/admin/images/logo.png') }}"
                     srcset="{{ asset('assets/admin/images/logo2x.png') }}" alt="logo">
                 <img class="logo-dark logo-img" src="{{ asset('assets/admin/images/logo-dark.png') }}"
@@ -22,64 +22,7 @@
         <div class="nk-sidebar-content">
             <div class="nk-sidebar-menu" data-simplebar>
                 <ul class="nk-menu">
-                    @if (Auth::user()->level->level_nama == 'admin')
-                        <li class="nk-menu-heading">
-                            <h6 class="overline-title text-primary-alt">Dashboards</h6>
-                        </li>
-                        <!-- .nk-menu-heading -->
-                        <li class="nk-menu-item">
-                            <a href="{{ route('admin.dashboard') }}" class="nk-menu-link">
-                                <span class="nk-menu-icon">
-                                    <em class="icon ni ni-dashlite"></em>
-                                </span>
-                                <span class="nk-menu-text">Dashboard</span>
-                            </a>
-                        </li>
-                        <!-- .nk-menu-item -->
-                        <li class="nk-menu-heading">
-                            <h6 class="overline-title text-primary-alt">Pages</h6>
-                        </li>
-                        <!-- .nk-menu-heading -->
-                        <li class="nk-menu-item has-sub">
-                            <a href="{{ route('mahasiswa.index') }}" class="nk-menu-link">
-                                <span class="nk-menu-icon">
-                                    <em class="icon ni ni-users"></em>
-                                </span>
-                                <span class="nk-menu-text">User Manage</span>
-                            </a>
-                            <!-- .nk-menu-sub -->
-                        </li>
-                    @endif
-                    @if (Auth::user()->level->level_nama == 'dosen')
-                        <li class="nk-menu-heading">
-                            <h6 class="overline-title text-primary-alt">Dashboards</h6>
-                        </li>
-                        <!-- .nk-menu-heading -->
-                        <li class="nk-menu-item">
-                            <a href="{{ route('dosen.dashboard') }}" class="nk-menu-link">
-                                <span class="nk-menu-icon">
-                                    <em class="icon ni ni-dashlite"></em>
-                                </span>
-                                <span class="nk-menu-text">Dashboard</span>
-                            </a>
-                        </li>
-                        <!-- .nk-menu-item -->
-                    @endif
-                    @if (Auth::user()->level->level_nama == 'mahasiswa')
-                        <li class="nk-menu-heading">
-                            <h6 class="overline-title text-primary-alt">Dashboards</h6>
-                        </li>
-                        <!-- .nk-menu-heading -->
-                        <li class="nk-menu-item">
-                            <a href="{{ route('mahasiswa.dashboard') }}" class="nk-menu-link">
-                                <span class="nk-menu-icon">
-                                    <em class="icon ni ni-dashlite"></em>
-                                </span>
-                                <span class="nk-menu-text">Dashboard</span>
-                            </a>
-                        </li>
-                        <!-- .nk-menu-item -->
-                    @endif
+                    @include('layouts.menu')
                     <!-- .nk-menu-item -->
                 </ul>
                 <!-- .nk-menu -->
