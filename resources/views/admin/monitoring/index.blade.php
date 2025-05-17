a2cf84cedf8762ed6155d64b4b66eb7ab33f9c35
FarrelCaesarian <farrelcaesarian15@gmail.com>
2025-05-17 10:51:18 +0700

    chart statistik

--- resources/views/admin/monitoring/index.blade.php
+++ resources/views/admin/monitoring/index.blade.php
@@ -6,10 +6,45 @@
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
-    <h6>Jumlah Mahasiswa Magang : {{$jumlahMahasiswaMagang}}</h6>
-    <br>
-    <h6>Jumlah Dosen Pembimbing : {{$jumlahDosenPembimbing}}</h6>
-    <br>
-    <h6>Rasio Dosen Pembimbing dibanding Mahasiswa : {{$rasio}}</h6>
+    <div class="row">
+        {{-- Card: Jumlah Mahasiswa Magang --}}
+        <div class="col-md-4">
+            <div class="card text-white bg-primary mb-3">
+                <div class="card-header">Mahasiswa Magang</div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $jumlahMahasiswaMagang }}</h5>
+                    <p class="card-text">Total mahasiswa yang sedang menjalani magang.</p>
+                </div>
+            </div>
+        </div>
+    
+        {{-- Card: Jumlah Dosen Pembimbing --}}
+        <div class="col-md-4">
+            <div class="card text-white bg-success mb-3">
+                <div class="card-header">Dosen Pembimbing</div>
+                <div class="card-body">
+                    <h5 class="card-title">{{ $jumlahDosenPembimbing }}</h5>
+                    <p class="card-text">Jumlah dosen pembimbing yang terdaftar.</p>
+                </div>
+            </div>
+        </div>
+    
+        {{-- Card: Rasio --}}
+        <h6 class="mb-2">Rasio Dosen : Mahasiswa</h6>
+            <div class="progress mb-3" style="height: 30px; " >
+                <div class="progress-bar bg-success" role="progressbar"
+                    style="width: {{ ($jumlahDosenPembimbing / ($jumlahMahasiswaMagang + $jumlahDosenPembimbing)) * 100 }}%;"
+                    aria-valuenow="{{ $jumlahDosenPembimbing }}" aria-valuemin="0" aria-valuemax="{{ $jumlahMahasiswaMagang + $jumlahDosenPembimbing }}">
+                    Dosen ({{ $jumlahDosenPembimbing }})
+                </div>
+                <div class="progress-bar bg-primary" role="progressbar"
+                    style="width: {{ ($jumlahMahasiswaMagang / ($jumlahMahasiswaMagang + $jumlahDosenPembimbing)) * 100 }}%;"
+                    aria-valuenow="{{ $jumlahMahasiswaMagang }}" aria-valuemin="0" aria-valuemax="{{ $jumlahMahasiswaMagang + $jumlahDosenPembimbing }}">
+                    Mahasiswa ({{ $jumlahMahasiswaMagang }})
+                </div>
+            </div>
+    
+    </div>
+    
 
 @endsection
